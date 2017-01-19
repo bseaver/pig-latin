64ae821e6e16c76757005cef98a453c4e849ac81
Benjamin T. Seaver and David Rolfs <david@>
2017-01-18 16:24:44 -0800

    impliment the first rule of the pig latin translation

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
   // Back end section
 
+  var toPigLatin = function(phrase) {
+    return phrase;
+  }
+
   // Front end section
   $("form").submit(function(event) {
     event.preventDefault();
+
+    // Get Input
+    var johnny = $("#inputPhrase").val();
+
+    // Get output from translator
+    var peter = toPigLatin(johnny);
+
+    // Put output into result span
+    $("#translation").text(peter);
+
   });
 
 }); // End Document Ready
